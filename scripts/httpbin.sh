e1487742c9050bc65c86cd374cb498d00f6649f7
Abhishek Tiwari <abhishek@abhishek-tiwari.com>
2024-01-07 22:39:16 +0900

    Fix kubectl refrence

--- scripts/httpbin.sh
+++ scripts/httpbin.sh
@@ -4,6 +4,6 @@ sudo -i -u `head -1 ./data/ssh.users`
 
 echo "$(<./data/httpbin.yaml)" >> /home/`head -1 ./data/ssh.users`/httpbin.yaml
 
-kubectl apply -f /home/`head -1 ./data/ssh.users`/httpbin.yaml
+micrkk8s kubectl apply -f /home/`head -1 ./data/ssh.users`/httpbin.yaml
 
 EOF
