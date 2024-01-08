3bb129eca9156939f8d189dfb7a4edcfb728f61f
Abhishek Tiwari <abhishek@abhishek-tiwari.com>
2024-01-08 18:28:29 +0900

    Added second k8s node for multipass

--- test.sh
+++ test.sh
@@ -5,4 +5,12 @@ echo "k8s-master-1 created"
 multipass transfer init.sh k8s-master-1:
 echo "Added init.sh to k8s-master-1"
 multipass exec k8s-master-1 -d /home/ubuntu -- sudo -i bash /home/ubuntu/init.sh
-echo "Finished running init.sh"
+echo "Finished running init.sh on k8s-master-1"
+multipass delete k8s-master-2
+multipass purge
+multipass launch focal --name k8s-master-2 -vvvv --timeout 600 -m 2G
+echo "k8s-master-2 created"
+multipass transfer init.sh k8s-master-2:
+echo "Added init.sh to k8s-master-2"
+multipass exec k8s-master-2 -d /home/ubuntu -- sudo -i bash /home/ubuntu/init.sh
+echo "Finished running init.sh on k8s-master-2"
