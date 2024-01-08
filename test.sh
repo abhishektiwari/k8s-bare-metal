4d634b0237e81792536e65c2f2908f5c02f0bb80
Abhishek Tiwari <abhishek@abhishek-tiwari.com>
2024-01-08 18:18:13 +0900

    Updated memory for multipass vm

--- test.sh
+++ test.sh
@@ -1,6 +1,6 @@
 multipass delete k8s-master-1
 multipass purge
-multipass launch focal --name k8s-master-1 -vvvv --timeout 600
+multipass launch focal --name k8s-master-1 -vvvv --timeout 600 -m 2G
 echo "k8s-master-1 created"
 multipass transfer init.sh k8s-master-1:
 echo "Added init.sh to k8s-master-1"
